bc454b358dc79afee38c1ad2e5c081a50d0f309c
DevSec <arma.devsec@gmail.com>
2016-12-31 23:10:23 +0100

    background

--- dialogs/routes/modal.js
+++ dialogs/routes/modal.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/:playerID?/:sessionID?/:title?/:text?/:icon?', function(req, res, next) {
+router.get('/:playerID?/:sessionID?/:title?/:text?/:icon?/:background?', function(req, res, next) {
     console.log(req.params);
     if (req.params.playerID !== undefined && req.params.playerID !== undefined) {
         res.render('modal', {
             title: req.params.title || 'Development Server',
             text: req.params.text ? JSON.parse(req.params.text) : ["This Server is currently in development.", " Only developers are arround here, not much to see here.", " want to continue"],
-            icon: req.params.icon || 'warning sign icon'
+            icon: req.params.icon || 'warning sign icon',
+            background: req.params.background || 'rgba(0, 0, 0, 0)'
         });
     } else {
         res.render('invalid');
