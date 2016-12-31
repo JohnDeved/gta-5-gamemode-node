cdcd9877595e453b8d840a98ad5f1c1d1225172a
DevSec <arma.devsec@gmail.com>
2016-12-31 19:48:43 +0100

    danke für die idee anton <3

--- dialogs/routes/index.js
+++ dialogs/routes/index.js
@@ -3,8 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    console.log(req.headers);
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'WebTest', data: req.headers });
 });
 
 module.exports = router;
