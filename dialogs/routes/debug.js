c21242f30f1344bc301e7fd4d624410ce2b0d9e9
DevSec <arma.devsec@gmail.com>
2017-01-01 02:56:50 +0100

    knöpfe

--- dialogs/routes/debug.js
+++ dialogs/routes/debug.js
@@ -4,6 +4,7 @@ var router = express.Router();
 router.get('/:playerID?/:sessionID?', function(req, res, next) {
     console.log(req.params);
     if (req.params.playerID !== undefined && req.params.playerID !== undefined) {
+        res.setHeader('Access-Control-Allow-Origin', 'http://185.62.188.120:3001/');
         res.render('debug', {
             playerID: req.params.playerID,
             sessionID: req.params.sessionID
