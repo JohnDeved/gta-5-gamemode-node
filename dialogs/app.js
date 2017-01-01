e8520d0a4be8628be971462fbeecfbadea5d276f
Scripticus <Anton.Kurth@gmx.de>
2017-01-01 08:46:36 +0100

    FIX: Gloabl

--- dialogs/app.js
+++ dialogs/app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mysql = require('mysql');
+var result = false;
+var done = false;
 
 mysqlVerify = (socialclub_id, session_id) => {
     result = false;
