9c8ccd1312f476efb10e278762010a646888533d
DevSec <arma.devsec@gmail.com>
2017-01-01 06:35:55 +0100

    is scheiß egal (object send)

--- dialogs/routes/debug.js
+++ dialogs/routes/debug.js
@@ -5,12 +5,28 @@ router.get('/:playerID?/:sessionID?', function(req, res, next) {
     console.log(req.params);
     if (req.params.playerID !== undefined && req.params.playerID !== undefined) {
         res.setHeader('Access-Control-Allow-Origin', 'http://185.62.188.120:3001/');
+        var toUnicode = (str) => {
+            newStr = ""
+            for (var i = 0; i < str.length; i++) {
+                newStr += str.charCodeAt(i) + ' '
+            }
+            return newStr
+        }
+
         res.render('debug', {
             playerID: req.params.playerID,
             sessionID: req.params.sessionID,
             buttons: {
-                execute: {},
-                close: {}
+                execute: {
+                    command: 'ADMIN_EVAL',
+                    args: toUnicode('$("#code").val()'),
+                    isFnc: true
+                },
+                close: {
+                    command: 'CEF_CLOSE',
+                    args: "debugCEF",
+                    isFnc: false
+                }
             }
         });
     } else {
