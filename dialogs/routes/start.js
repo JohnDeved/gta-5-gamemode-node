d59ebecdd1f92d0b6a8206fb571a0fc9fe01d159
DevSec <arma.devsec@gmail.com>
2017-01-01 21:17:56 +0100

    ADD: start check
    + beide butten closen fürs erste

--- dialogs/routes/start.js
+++ dialogs/routes/start.js
@@ -3,13 +3,58 @@ var router = express.Router();
 
 router.get('/:playerID?/:sessionID?', function(req, res, next) {
     console.log(req.params);
-    if (req.params.playerID !== undefined && req.params.playerID !== undefined) {
-        res.render('start', {
-
-        });
-    } else {
-        res.render('invalid');
+    var toUnicode = (str) => {
+        newStr = ""
+        for (var i = 0; i < str.length; i++) {
+            newStr += str.charCodeAt(i) + ' '
+        }
+        return newStr
     }
+    mysqlVerify = (socialclub_id, session_id) => {
+      request({
+          url: 'http://185.62.188.120:3001/VerifyUser',
+          method: 'post',
+          form: {
+            socialclub_id: socialclub_id,
+            session_id: session_id
+          }
+      }, function (error, response, body) {
+          if(error) {
+              mysqlCallback(false);
+          } else {
+              console.log(response.statusCode, body);
+              mysqlCallback(body == "1");
+          }
+      });
+      return true;
+    }
+    mysqlCallback = (result) => {
+        console.log("result is",result);
+        if (result) {
+            res.setHeader('Access-Control-Allow-Origin', 'http://185.62.188.120:3001/');
+
+            res.render('debug', {
+                playerID: req.params.playerID,
+                sessionID: req.params.sessionID,
+                buttons: {
+                    abbrechen: {
+                        command: 'CEF_CLOSE',
+                        args: "startCEF",
+                        isFnc: false
+                    },
+                    erstellen: {
+                        command: 'CEF_CLOSE',
+                        args: "startCEF",
+                        isFnc: false
+                    }
+                }
+            });
+        } else {
+            console.log("ERROR: ",result);
+            res.render('invalid');
+        }
+    }
+    mysqlVerify(req.params.playerID, req.params.sessionID)
 });
 
 module.exports = router;
