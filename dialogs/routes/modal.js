b66ee3b3519a6f115e869c9f95bec5d307807ae3
Scripticus <Anton.Kurth@gmx.de>
2017-01-02 21:19:21 +0100

    ADD: Modal-Content-Args akzeptieren

--- dialogs/routes/modal.js
+++ dialogs/routes/modal.js
@@ -2,12 +2,12 @@ var express = require('express');
 var request = require('request');
 var router = express.Router();
 
-router.get('/:playerID?/:sessionID?', function(req, res, next) {
+router.get('/:playerID?/:sessionID?/:title?/:text?', function(req, res, next) {
     GTARequest(router, req, res, 'modal', {
         playerID: req.params.playerID,
         sessionID: req.params.sessionID,
         title: req.params.title || 'Development Server',
-        text: req.params.text ? JSON.parse(req.params.text) : ["This Server is currently in development.", " Only developers are arround here, not much to see here.", " want to continue"],
+        text: req.params.text.split('*'),
         icon: req.params.icon || 'warning sign icon',
         background: req.params.background || 'rgba(0, 0, 0, 0)',
         buttons: {
