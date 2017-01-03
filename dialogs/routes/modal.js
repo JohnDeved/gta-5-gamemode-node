080f1be6c5faee5e44d6483568778fb189413853
DevSec <arma.devsec@gmail.com>
2017-01-03 02:08:54 +0100

    FIX: array

--- dialogs/routes/modal.js
+++ dialogs/routes/modal.js
@@ -7,7 +7,7 @@ router.get('/:playerID?/:sessionID?/:title?/:text?', function(req, res, next) {
         playerID: req.params.playerID,
         sessionID: req.params.sessionID,
         title: req.params.title || 'Development Server',
-        text: req.params.text ? req.params.text.split('*') : 'sample text',
+        text: req.params.text ? req.params.text.split('*') : ['sample text'],
         icon: req.params.icon || 'warning sign icon',
         background: req.params.background || 'rgba(0, 0, 0, 0)',
         buttons: {
